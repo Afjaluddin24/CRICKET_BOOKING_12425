ae23e59ee11af14d17df63d3002f5be8b2441b9f
Afjaluddin24 <shekhafjal024@gmail.com>
2025-05-02 14:15:51 +0530

    Update all file

--- CRICKET_BOOKING_12425/Controllers/API/DashboardController.cs
+++ CRICKET_BOOKING_12425/Controllers/API/DashboardController.cs
@@ -22,20 +22,17 @@ namespace CRICKET_BOOKING_12425.Controllers.API
         {
             try
             {
-                var admin = await _dbContext.AdminMasters.FindAsync(AdminMasterId);
+                var Tournament = await _dbContext.Tournaments.CountAsync();
+                var Teams = await _dbContext.BookingsTeams.CountAsync();
+                var Match = await _dbContext.CricketMatches.CountAsync();
+                var News = await _dbContext.News.CountAsync();
 
-                if (admin != null)
-                {
-                    var Tournament = await _dbContext.Tournaments.Select(t => t.AdminMasterId).Distinct().CountAsync();
-                    var Teams = await _dbContext.BookingsTeams.Select(o => o.AdminMasterId).Distinct().CountAsync();
-                    var Match = await _dbContext.CricketMatches.Select(o => o.AdminMasterId).Distinct().CountAsync();
+                //var Tournament = await _dbContext.Tournaments.Select(t => t.AdminMasterId == AdminMasterId).Distinct().CountAsync();
+                //    var Teams = await _dbContext.BookingsTeams.Select(o => o.AdminMasterId == AdminMasterId).Distinct().CountAsync();
+                //    var Match = await _dbContext.CricketMatches.Select(o => o.AdminMasterId == AdminMasterId).Distinct().CountAsync();
 
-                    return Ok(new { Status = "Ok", Result = new { Tournament, Teams, Match } });
-                }
-                else
-                {
-                    return Ok(new { Status = "Fail", Result = "Admin not found" });
-                }
+                return Ok(new { Status = "Ok", Result = new { Tournament, Teams, Match, News } });
+               
             }
             catch (Exception ex)
             {
